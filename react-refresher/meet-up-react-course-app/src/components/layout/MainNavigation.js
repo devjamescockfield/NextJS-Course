87917eff9d887789f3161fb8dcfd65586287bb69
James Cockfield <jamescockfield10@gmail.com>
2023-07-29 21:03:40 +0100

    Scoping component styles with CSS Modules

--- react-refresher/meet-up-react-course-app/src/components/layout/MainNavigation.js
+++ react-refresher/meet-up-react-course-app/src/components/layout/MainNavigation.js
@@ -1,18 +1,22 @@
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
+
+import classes from "./MainNavigation.module.css";
 
 function MainNavigation() {
-    return <header>
-        <div>React Meetups</div>
-        <nav>
-            <ul>
-                <li>
-                    <Link to='/'>All Meetups</Link>
-                    <Link to='/new-meetup'>New Meetups</Link>
-                    <Link to='/favorites'>My Favorites</Link>
-                </li>
-            </ul>
-        </nav>
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>React Meetups</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">All Meetups</Link>
+            <Link to="/new-meetup">New Meetups</Link>
+            <Link to="/favorites">My Favorites</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
+  );
 }
 
 export default MainNavigation;
