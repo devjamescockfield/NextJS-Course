83f7c9b5e5caf2ef74aa08873e6fbae81ed3f1b6
James Cockfield <jamescockfield10@gmail.com>
2023-07-29 21:22:46 +0100

    add more components

--- react-refresher/meet-up-react-course-app/src/pages/AllMeetups.js
+++ react-refresher/meet-up-react-course-app/src/pages/AllMeetups.js
@@ -1,3 +1,5 @@
+import MeetupList from "../components/meetups/MeetupList";
+
 const DUMMY_DATA = [
   {
     id: "m1",
@@ -33,9 +35,7 @@ function AllMeetupsPage() {
     <div>
       <h1>All Meetups</h1>
       <ul>
-        {DUMMY_DATA.map((meetup) => {
-          return <li key={meetup.id}>{meetup.title}</li>;
-        })}
+        <MeetupList meetups={DUMMY_DATA} />
       </ul>
     </div>
   );
